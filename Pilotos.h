e4f92296b4bb6d3f39f36798d2ec86fc96956a1e
Guilherme Henrique <a226881906@fumec.edu.br>
2022-05-05 10:57:12 -0300

    push

---
+++ Pilotos.h
@@ -0,0 +1,12 @@
+struct pilotos
+{
+  char nacionalidade[30];
+  char corPele[30];
+  char corOlhos[30];
+  char corCabelo[30];
+  long int altura;
+  int idade;
+  int QtdCampeonatosGanhos;
+  int polePosition;				// Posição de iniciação na corrida.
+  int QtdVoltasRap;				// Quantidade de voltas mais rápidas.
+}
