1342b51845c0c99478f296bc001be3ee8fa96cf1
KarenPSMoreira <95109398+KarenPSMoreira@users.noreply.github.com>
2022-05-12 10:49:40 -0300

    Update carro.h

--- carro.h
+++ carro.h
@@ -1,20 +1,54 @@
-#include <stdio.h>
-#include <string.h>
+typedef struct carro;
 
 struct carro {
-    char nome[50];
+    char nome[51];
     float comprimento;
     float largura;
     float altura;
     float peso;
+    char marcaMotor[31];
+    char tipoMotor[31];
     float aceleracao;
     float capacidadeCombustivel;
     int caixaCambio;
     int aroRoda;
     float qtdEtanol;
-
-    struct carroMotor {
-        char marca[30];
-        char tipo[30];
-    }
 }
+	
+    
+printf("Nome do carro:");
+fgets(nome, 51, stin);
+    
+printf("\nComprimento do carro:");
+scanf("%f",comprimento);
+    
+printf("\nLargura do carro:");
+scanf("%f",largura);
+    
+printf("\nAltura do carro:");
+scanf("%f",alturao);
+    
+printf("\nPeso do carro:");
+scanf("%f",peso);
+    
+printf("Marca do motor:");
+fgets(marcaMotor, 31, stdin );
+    
+printf("Tipo de motor:");
+fgets(tipoMotor, 31, stdin );
+    
+printf("\nAceleração (0 a 100 km/h):");
+scanf("%f",aceleracao);
+    
+printf("\nCapacidade do tanque de combustivel:");
+scanf("%f",capacidadeCombustivel);
+    
+printf("\nCaixa de cambio:");
+scanf("%i",caixaCambio);
+    
+printf("\nAro da roda:");
+scanf("%i",aroRoda);
+    
+printf("\nQuantidade de etanol no combustivel:");
+scanf("%f",qtdEtanol);
+    
