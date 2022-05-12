9de47d7cb325dc6c4d2b4468efbb4e91a95a7126
KarenPSMoreira <95109398+KarenPSMoreira@users.noreply.github.com>
2022-05-12 11:01:16 -0300

    Update piloto.h

--- piloto.h
+++ piloto.h
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <string.h>
+typedef struct piloto;
 
 struct piloto {
     char nome[50];
@@ -7,13 +6,51 @@ struct piloto {
     char corPele[30];
     char corOlhos[30];
     char corCabelo[30];
-    long int altura;
+    long altura;
     int peso;
     int idade;
     int qtdCampeonatosGanhos;
     int polePosition;				// Posição de iniciação na corrida.
-    int qtdVoltasRap;				// Quantidade de voltas mais rápidas.
-}
+    int qtdVoltasRap;      			// Quantidade de voltas mais rápidas
+    
+}				.
+    
+printf("----------------------------------PILOTO-----------------------------------------");
+	    
+printf("\nInsira seu nome: ");
+fgets(nome, 50, stdin);
+    
+printf("\nInsira sua nacionalidade: ");
+fgets(nacionalidade, 30, stdin);
+    
+printf("\nInsira a cor da pele: ");
+fgets(corPele, 30, stdin);
+    
+printf("\nInsira a cor dos olhos: ");
+fgets(corOlhos, 30, stdin);
+    
+printf("\nInsira a cor do cabelo: ");
+fgets(corCabelo, 30, stdin);
+    
+printf("\nInsira sua altura: ");
+scanf("%d", &altura);
+    
+printf("\nInsira seu peso: ");
+scanf("%d", &peso);
+    
+printf("\nInsira sua idade: ");
+scanf("%d", &idade);
+    
+printf("\nInsira a quantidade de capeonatos ganhos: ");
+scanf("%d", &qtdCampeonartosGanhos);
+    
+printf("\nInsira o pole position: ");
+scanf("%d", &polePosition);
+    
+printf("\nInsira a quantidade de voltas mais rapidas: ");
+scanf("%d", &qtdVoltasRap);
+
+/*
 
 // Getting and Setting for Nome:
     void setNome() {
@@ -42,3 +79,5 @@ struct piloto {
 
         return pointer;
     }
+    
+    */
