395785adf8185c2b0a6e9ec0a30a4bb3bea05bb5
Karen Moreira <95109398+KarenPSMoreira@users.noreply.github.com>
2022-05-16 10:57:06 -0300

    Update menu.c

--- menu.c
+++ menu.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
-#include "piloto.h"
+#include <string.h>
+#include <stdlib.h>
+#include "Methods.h"
 
-// Getter and Setter for Nacionalidade
-	typedef void setPilotoNacionalidade();
-	typedef char getPilotoNacionalidade();
+int main(void) // FUNÇÃO  MENU
+{
+  int count;
+  
+  do{
+    
+     printf("\n\n");    
 
-int main() {
-	int cont;
-
-	do{
-		printf("\nMENU\n");
-        printf("1. Atribuir\n");
-        printf("2. Editar\n");
-        printf("3. Exluir\n");
-        printf("4. Imprimir\n");
-        printf("5. EhCraque\n");
-        printf("6. Sair\n");
-
-        scanf("%d", &cont);
-        
-        switch(cont){
+     printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c\n",3017,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3003);
+     printf("\t\t\t%c    Formula 1       %c\n",3002,3002);
+     printf("\t\t\t%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c%c",3016,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3021,3004);
+     
+     printf("\t\t  1  %c   CADASTRAR\n\n\t\t  2  %c   EDITAR\n\n\t\t  3  %c   EXCLUIR\n\n\t\t  4  %c   LISTAR\n\n\t\t  5  %c   PODIO\n\n\t\t  6  %c   SAIR\n\n",16,16,16,16,16,16,16,136,16);
+     printf("\t\t ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~   \n\t\t  %c     ",26);
+          
+    
+     scanf("\n%d",&count);
+     
+     switch(count){
 			case 1:
-				//atribuir();
-        		break;
+			CADASTRAMENTO_EQUIPE();
+        	break;
         	
 			case 2: 
 			    //editar();
@@ -43,12 +45,11 @@ int main() {
 			case 6:
 				//sair();
 				break;
-			
-			default:
-			  	printf("\n Digite uma opcao valida: \n");    
-			    break;
 		}
-	}while(cont);
+			
+   
+     }while((count<1) || (count>8));
+    
+ return 0;
 
-	return 0;
 }
