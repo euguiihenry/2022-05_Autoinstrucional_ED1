515cf7779871598cc0f3ba7eeb2075876008a578
Guilherme Henrique <a226881906@fumec.edu.br>
2022-05-25 02:18:21 -0300

    Update piloto.h

--- piloto.h
+++ piloto.h
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-void cadastramento_piloto();
-void excluir_piloto();
-void abrir();
-void fechar();
 
-// Variables:
-	FILE *filePointer;
+// Functions Loading:
+	void cadastramento_piloto();
+	void excluir_piloto();
+	void abrir();
+	void criar();
+	void fechar();
 
 // Estrutura:
 	struct piloto {
@@ -16,106 +16,156 @@ void fechar();
 	    char corPele[30];
 	    char corOlhos[30];
 	    char corCabelo[30];
-	    long altura;
+	    int altura;
 	    int peso;
 	    int idade;
 	    int qtdCampeonatosGanhos;
-	    int polePosition;				// PosiÃ§Ã£o de iniciaÃ§Ã£o na corrida.
-	    int qtdVoltasRap;      			// Quantidade de voltas mais rÃ¡pidas
+	    int polePosition;				// Posicao de iniciacao na corrida.
+	    int qtdVoltasRap;      			// Quantidade de voltas mais rapidas
 
 	};
 
-// FunÃ§Ãµes:
+// Variables:
+	FILE *filePointer;
+	struct piloto *pilotoPointer;
+	int length;
+
+
+// Funcoes:
 	/* Handling with Files: 
 	==========================================================================================================*/
 		void abrir() {
-			//fopen("fileopen","mode");
-			filePointer = fopen("./TextFiles/pilotos.txt", "w");
+			filePointer = fopen("pilotos.txt", "r+");
+			
+			if(filePointer == NULL) {
+				criar();
+				fechar();
+				abrir();          
+			}
+		}
+		
+		void criar() {
+			filePointer = fopen("pilotos.txt", "w");
 
 			if(filePointer == NULL) {
-				printf("Error!");   
+				printf("Erro ao criar o arquivo!");   
 				exit(1);             
 			}
 		}
 
 		void fechar() {
-			//fclose()
 			fclose(filePointer);
 		}
 
 	/* Cadastramento do Piloto:
 	==========================================================================================================*/
 		void cadastramento_piloto() {
+			int n; // Para uso no loop do FOR.
+			
+			// Pegando o numero de inserções:
+				printf("\n\t\t Insira o numero de pilotos deseja cadastrar: ");
+				scanf("%i", &length);
+			
 			// Definindo a estrutura a ser utilizada e seu apelido:
-				struct piloto p;
+				struct piloto p[length];
+				
+			// Definindo um ponteiro para p[]:
+				pilotoPointer = &p[0];
 			
 			// Criando/Abrindo o Arquivo TXT:
 				abrir();
-			
-			// Limpando o buffer do teclado:
-				fflush(stdin);
 				
-			// ImpressÃ£o de qual tarefa serÃ¡ realizada:
-				printf("\n\t\t ======================== CADASTRAMENTO DE PILOTO ========================\n");
+			// Setando o ponteiro ao final do arquivo:
+				fseek(filePointer, 0, SEEK_END);
+				
+			// Impressao de qual tarefa sera realizada:
+				printf("\n\t\t ================= CADASTRAMENTO DE PILOTO =================\n");
 			
-			// InserÃ§Ãµes:
-				// Nome:
-					printf("\t\t Insira o nome do piloto: ");
-					fgets(p.nome, 50, stdin);
+			// Insercoes:
+				// Coleta de dados:
+					for (n=0; n<length; n++) {
+						// Limpando o buffer do teclado:
+							fflush(stdin);
+							
+						// Nome:
+							printf("\t\t Insira o nome do piloto: ");
+							fgets(p[n].nome, 50, stdin);
+							
+						// Nacionalidade:
+							printf("\t\t Insira a nacionalidade do piloto: ");
+							fgets(p[n].nacionalidade, 30, stdin);
+							
+						// Cor de Pele:
+							printf("\t\t Insira a cor de pele do piloto: ");
+							fgets(p[n].corPele, 30, stdin);
+							
+						// Cor dos Olhos:
+							printf("\t\t Insira a cor dos olhos do piloto: ");
+							fgets(p[n].corOlhos, 30, stdin);
+							
+						// Cor do Cabelo:
+							printf("\t\t Insira a cor do cabelo do piloto: ");
+							fgets(p[n].corCabelo, 30, stdin);
+							
+						// Altura:
+							printf("\t\t Insira a altura do piloto: ");
+							scanf("%i", &p[n].altura);
+							
+						// Peso:
+							printf("\t\t Insira o peso do piloto: ");
+							scanf("%i", &p[n].peso);
+							
+						// Idade:
+							printf("\t\t Insira a idade do piloto: ");
+							scanf("%i", &p[n].idade);
+							
+						// Qtd de Campeonatos Ganhos:
+							printf("\t\t Insira a quantidade de campeonatos ganhos pelo piloto: ");
+							scanf("%i", &p[n].qtdCampeonatosGanhos);
+							
+						// Pole Position:
+							printf("\t\t Insira a posicao de largada do piloto: ");
+							scanf("%i", &p[n].polePosition);
+							
+						// Qtd de Voltas Mais Rapidas:
+							printf("\t\t Insira a quantidade de voltas mais rapidas feitas pelo piloto: ");
+							scanf("%i", &p[n].qtdVoltasRap);
+							
+						// Pulando uma linha:
+							printf("\n");
+					}
 					
-				// Nacionalidade:
-					printf("\t\t Insira a nacionalidade do piloto: ");
-					fgets(p.nacionalidade, 30, stdin);
-					
-				// Cor de Pele:
-					printf("\t\t Insira a cor de pele do piloto: ");
-					fgets(p.corPele, 30, stdin);
-					
-				// Cor dos Olhos:
-					printf("\t\t Insira a cor dos olhos do piloto: ");
-					fgets(p.corOlhos, 30, stdin);
-					
-				// Cor do Cabelo:
-					printf("\t\t Insira a cor do cabelo do piloto: ");
-					fgets(p.corCabelo, 30, stdin);
-					
-				// Altura:
-					printf("\t\t Insira a altura do piloto: ");
-					scanf("%i", &p.altura);
-					
-				// Peso:
-					printf("\t\t Insira o peso do piloto: ");
-					scanf("%i", &p.peso);
-					
-				// Idade:
-					printf("\t\t Insira a idade do piloto: ");
-					scanf("%i", &p.idade);
-					
-				// Qtd de Campeonatos Ganhos:
-					printf("\t\t Insira a quantidade de campeonatos ganhos pelo piloto: ");
-					scanf("%i", &p.qtdCampeonatosGanhos);
-					
-				// Pole Position:
-					printf("\t\t Insira a posiÃ§Ã£o de largada do piloto: ");
-					scanf("%i", &p.polePosition);
-					
-				// Qtd de Voltas Mais Rapidas:
-					printf("\t\t Insira a quantidade de voltas mais rÃ¡pidas feitas pelo piloto: ");
-					scanf("%i", &p.qtdVoltasRap);
-					
-				// Integrando os dados ao arquivo:
-					fprintf(filePointer, "%s", p.nome);
+				// Adicao ao arquivo txt:
+					for (n=0; n<length; n++) {
+						fprintf(filePointer, "%s", p[n].nome);
+						fprintf(filePointer, "%s", p[n].nacionalidade);
+						fprintf(filePointer, "%s", p[n].corPele);
+						fprintf(filePointer, "%s", p[n].corOlhos);
+						fprintf(filePointer, "%s", p[n].corCabelo);
+						fprintf(filePointer, "%i\n", p[n].altura);
+						fprintf(filePointer, "%i\n", p[n].peso);
+						fprintf(filePointer, "%i\n", p[n].idade);
+						fprintf(filePointer, "%i\n", p[n].qtdCampeonatosGanhos);
+						fprintf(filePointer, "%i\n", p[n].polePosition);
+						fprintf(filePointer, "%i\r\n", p[n].qtdVoltasRap);
+					}
 					
 				//Fechando o arquivo TXT:
-					fechar();
-						
+					fechar();		
 		}
 	
 	// Edicao do Piloto:
-		
+		void edicao_piloto() {			
+			// Impressão do array:
+				int n;
+				
+				for(n=0; n<length; n++) {
+					printf("%s\n%s\n%s\n%s\n%s\n%i\n%i\n%i\n%i\n%i\n%i\r\n", *(pilotoPointer+n).nome, *((pilotoPointer+n).nacionalidade), *((pilotoPointer+n).corPele), p.corOlhos, p.corCabelo, p.altura, p.peso, p.idade, p.qtdCampeonatosGanhos, p.polePosition, p.qtdVoltasRap );
+				}
+		}
 	
 	// Exclusao do Piloto:
-void excluir_piloto(void){
+/*void excluir_piloto(void){
 	
 	int i,TAM=100, continuar;
 	char nome_busca[50];
@@ -232,5 +282,5 @@ void EhCraque(){
 			
 		}
 		
-	}
+	}*/
 		
