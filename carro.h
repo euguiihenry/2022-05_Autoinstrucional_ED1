6a89d6349228657a856ea599c6d7a67e71595fc6
arthurantunes1210 <105174082+arthurantunes1210@users.noreply.github.com>
2022-05-24 21:12:19 -0300

    Add files via upload

--- carro.h
+++ carro.h
@@ -1,54 +1,132 @@
-typedef struct carro;
-
-struct carro {
-    char nome[51];
-    float comprimento;
-    float largura;
-    float altura;
-    float peso;
-    char marcaMotor[31];
-    char tipoMotor[31];
-    float aceleracao;
-    float capacidadeCombustivel;
-    int caixaCambio;
-    int aroRoda;
-    float qtdEtanol;
-}
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void cadastroCarro();
+void abrir();
+void fechar();
 	
+	//declaracao de variaveis
+    FILE *filePointer;
+
+// estrutura
+	struct carro {
+   		char nome[51];
+    	float comprimento;
+    	float largura;
+    	float altura;
+    	float peso;
+    	char marcaMotor[31];
+    	char tipoMotor[31];
+    	float aceleracao;
+    	float capacidadeCombustivel;
+    	int caixaCambio;
+    	int aroRoda;
+    	float qtdEtanol;
+		}
+	
+           //funcoes
+			void abrir(){
+				
+				filePointer = fopen("./TextFiles/carro.txt", "w");
+				
+				if(filePointer == NULL){
+					printf("ERROR!");
+					exit(1);
+				}
+			}
+		
+			void fechar(){
+				
+		 	    fclose(filePoiter);
+			}
+	
+	    //cadastramento do carro
+        void cadastroCarro(){
+		
+		    //declaranco um apelido
+				struct carro c;
+				
+			//metodo para abrir o arquivo txt	
+				abrir();
+				
+			// limpando a memoria
+				fflush(stdin);
+				
+				// impressao de qual item sera chamado
+				printf("\n\t\t =============== CADASTRAMENTO DO CARRO =================\n");
+				
+				
+				// nome
+					printf("Nome do carro:");		
+					fgets(c.nome, 51, stin);
+					
+					
+                // comprimento do carro
+					printf("\nComprimento do carro:");
+					scanf("%f", &c.comprimento);
     
-printf("Nome do carro:");
-fgets(nome, 51, stin);
     
-printf("\nComprimento do carro:");
-scanf("%f",comprimento);
+               // largura do carro
+					printf("\nLargura do carro:");
+					scanf("%f", &c.largura);
     
-printf("\nLargura do carro:");
-scanf("%f",largura);
     
-printf("\nAltura do carro:");
-scanf("%f",alturao);
+                // altura do carro
+					printf("\nAltura do carro:");
+					scanf("%f", &c.altura);
+					
+					
+                //peso do carro
+					printf("\nPeso do carro:");
+					scanf("%f", &c.peso);
     
-printf("\nPeso do carro:");
-scanf("%f",peso);
     
-printf("Marca do motor:");
-fgets(marcaMotor, 31, stdin );
+                //marca do carro
+					printf("Marca do motor:");
+					fgets(c.marcaMotor, 31, stdin );
     
-printf("Tipo de motor:");
-fgets(tipoMotor, 31, stdin );
     
-printf("\nAceleração (0 a 100 km/h):");
-scanf("%f",aceleracao);
+               //Tipo de motor
+					printf("Tipo de motor:");
+					fgets(c.tipoMotor, 31, stdin );
+   
+   
+               //Aceleracao 
+					printf("\nAceleração (0 a 100 km/h):");
+					scanf("%f", &c.aceleracao);
     
-printf("\nCapacidade do tanque de combustivel:");
-scanf("%f",capacidadeCombustivel);
     
-printf("\nCaixa de cambio:");
-scanf("%i",caixaCambio);
+                //capacidade do tanque
+					printf("\nCapacidade do tanque de combustivel:");
+					scanf("%f", &c.capacidadeCombustivel);
     
-printf("\nAro da roda:");
-scanf("%i",aroRoda);
     
-printf("\nQuantidade de etanol no combustivel:");
-scanf("%f",qtdEtanol);
+               // caixa de cambio
+					printf("\nCaixa de cambio:");
+					scanf("%i", &c.caixaCambio);
     
+    
+               //aro da roda
+					printf("\nAro da roda:");
+					scanf("%i", &c.aroRoda);
+    
+    
+    	       //quantidade de etanol 
+					printf("\nQuantidade de etanol no combustivel:");
+					scanf("%f", &c.qtdEtanol);
+					
+				//integrando od dados do arquvo
+					fprintf(filePointer, "%s", c.nome);
+					
+				//fechando o arquivo TXT
+					fechar();
+					
+					
+// edicao do carro:			
+
+//enclusao do carro:
+
+
+// Impressao do carro:		
+					
+}
